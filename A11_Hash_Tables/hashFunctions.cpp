55248c2e8fbdfc04eaea0371ae0500866240e34a
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2021-04-15 15:52:31 -0400

    A11

--- A11_Hash_Tables/hashFunctions.cpp
+++ A11_Hash_Tables/hashFunctions.cpp
@@ -17,100 +17,137 @@ using namespace std;
 // prototypes
 void print_menu();
 char get_command();
-size_t hash1(int key_value);
-void intialize_empty_table(int Table[][2]);
-void print_table(int Table[][2]);
+void hash1(int keys[50], int Table[50][2], size_t CAPACITY_X, size_t CAPACITY_Y);
+void intialize_empty_table(int Table[50][2]);
+void print_table(int Table[50][2], size_t const CAPACITY_X, size_t const CAPACITY_Y);
+void sumProbes(int Table[50][2], size_t const CAPACITY_X);
 
 int main()
 {
-    int keys[] = { 1234, 8234, 7867, 1009, 5438, 4312, 3420, 9487, 5418, 5299, 5078, 8239, 1208, 5098, 5195, 5329, 4543,
-	3344, 7698, 5412, 5567, 5672, 7934, 1254, 6091, 8732, 3095, 1975, 3843, 5589, 5439, 8907, 4097, 3096, 4310,
-	5298, 9156, 3895, 6673, 7871, 5787, 9289, 4553, 7822, 8755, 3398, 6774, 8289, 7665, 5523 };
-	
-	int Table[50][2];
-	size_t const CAPACITY_X = 50;
-	size_t const CAPACITY_Y = 2;
-	char command;
-	size_t hash_value;
-	
-	
-	do
-	{
-		print_menu();
-		command = get_command();
-		
-		if(command == '1')
-		{
-			intialize_empty_table(Table);
-			
-			for(size_t row = 0; row < CAPACITY_X; row++)
-			{
-				for(size_t column = 0; column < CAPACITY_Y; column++)
-				{
-					
-					//hash_value = hash1(keys[row]);
-					//cout << hash_value << endl;
-				} 
-			}
-			
-			hash_value = hash1(keys[0]);
-			
-			cout << "Printing" << endl;
-			print_table(Table);
-		}
-		
-		if(command == '2')
-		{
-			
-		}
-		
-		if(command == '3')
-		{
-			
-		}
-		
-		
-		
-	}while(command != '0');
+    int keys[] = { 1234, 8234, 7867, 1009, 5438, 4312, 3420, 9487, 5418, 5299, 5078, 8239, 1208, 5098, 5195, 5329,
+	4543, 3344, 7698, 5412, 5567, 5672, 7934, 1254, 6091, 8732, 3095, 1975, 3843, 5589, 5439, 8907, 4097, 3096,
+	4310, 5298, 9156, 3895, 6673, 7871, 5787, 9289, 4553, 7822, 8755, 3398, 6774, 8289, 7665, 5523 };
+
+    int Table[50][2];
+    size_t const CAPACITY_X = 50;
+    size_t const CAPACITY_Y = 2;
+    char command;
+
+    do {
+	print_menu();
+	command = get_command();
+
+	if(command == '1') {
+	    intialize_empty_table(Table);
+	    hash1(keys, Table, CAPACITY_X, CAPACITY_Y);
+	    print_table(Table, CAPACITY_X, CAPACITY_Y);
+	    sumProbes(Table, CAPACITY_X);
+	}
+
+	if(command == '2') {
+	}
+
+	if(command == '3') {
+	}
+
+    } while(command != '0');
     return EXIT_SUCCESS;
 }
 
-size_t hash1(int key_value)
+void hash1(int keys[50], int Table[50][2], size_t CAPACITY_X, size_t CAPACITY_Y)
 {
-	size_t hash_value;
-	
-	hash_value = key_value % 100;
-	cout << hash_value << endl;
-	
-	return hash_value;
-}
+    size_t hash_value = 0;
+    int key_value = 0;
 
-//fill in hash table with -1 to indicate unused
-void intialize_empty_table(int Table[][2])
-{
-	size_t column = 0;
-	for(size_t row = 0; row < 50; row++)
-	{
-		Table[row][column] = -1;
+    size_t current = 0;
+    size_t probe = 0;
+    int probe_counter = 0;
+    bool isVacant = true;
+    do {
+	// getting the key value for the first index and then hashing it.
+	key_value = keys[current];
+	hash_value = key_value % 50;
+	probe_counter = 0;
+	/*NOTE: probe counter will start out as 0 for every time the we hash a new value. When we probe,
+	then probe_counter will increment and be stored in Table[Hash_value][1]
+	note that in the else statement where the probe happens, hash_value is replaced in Table[probe][1]*/
+
+	//cout << "Key VALUE " << key_value << endl;
+	//cout << "HASH VALUE " << hash_value << endl;
+	//cout << endl;
+
+	// if the table at the hash value is empty, then we can put the key value in that spot
+	if(Table[hash_value][0] == -1) {
+	    Table[hash_value][0] = key_value;
+	    Table[hash_value][1] = probe_counter;
+	} else {
+	    // if the hash value is occupied, then we now start probing from the current position to the next position.
+	    probe = hash_value;
+	    isVacant = 1;
+		
+	    while(isVacant) {
+			
+		// if he probe has reach the end of the Table, then start the probe at index 0
+		if(probe > CAPACITY_X) {
+		    probe = 0;
+		}
+			probe++;
+			probe_counter++;
+		if(Table[probe][0] == -1) {
+		    Table[probe][0] = keys[current];
+			Table[probe][1] = probe_counter;
+		    isVacant = false;
+		}
+		
+	    }
 	}
-	
+	current++;
+    } while(current < CAPACITY_X);
 }
 
-void print_table(int Table[][2])
+// fill in hash table with -1 to indicate unused
+void intialize_empty_table(int Table[50][2])
 {
-	size_t column = 0;
-	
-	cout << "Index\t" << "Key\t" << "Probes\t" << endl;
-	cout << "-------------------------------" << endl;
-	for(size_t row = 0; row < 50; row++)
-	{
-		cout << row << "\t";
-		cout << Table[row][column] << "\t";
-		//missing probes
-		cout << endl;
-	}
+    //    size_t column = 0;
+    //    for(size_t row = 0; row < 50; row++) {
+    //	Table[row][column] = -1;
+    //    }
+
+    for(size_t row = 0; row < 50; row++) {
+
+	Table[row][0] = -1;
+	Table[row][1] = 0;
+    }
 }
 
+void print_table(int Table[][2], size_t const CAPACITY_X, size_t const CAPACITY_Y)
+{
+    // size_t column = 0;
+
+    cout << "\nIndex\t"
+         << "Key\t"
+         << "Probes\t" << endl;
+    cout << "-------------------------------" << endl;
+    for(size_t row = 0; row < CAPACITY_X; row++) {
+	cout << row << "\t";
+	for(size_t column = 0; column < CAPACITY_Y; column++) {
+	    cout << Table[row][column] << "\t";
+	}
+	cout << endl;
+    }
+}
+
+void sumProbes(int Table[50][2], size_t const CAPACITY_X)
+{
+    size_t sum = 0;
+
+    for(size_t row = 0; row < CAPACITY_X; row++) {
+	sum += Table[row][1];
+    }
+
+    cout << "\nSUM OF PROBES IS: " << sum;
+	cout << endl;
+}
 void print_menu()
 {
     cout << "\n-----Press a number for the following menu option-----\n" << endl;
