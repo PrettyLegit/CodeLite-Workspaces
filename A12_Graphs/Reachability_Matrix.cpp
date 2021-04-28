2593a34a0a87e7aac2346c73c6a7e490b6f38f3d
Jimmy Nguyen <nvjimmy27@gmail.com>
2021-04-28 18:32:27 -0400

    Updates

--- A12_Graphs/Reachability_Matrix.cpp
+++ A12_Graphs/Reachability_Matrix.cpp
@@ -14,25 +14,76 @@
 
 using namespace std;
 
+void matrix_multiplication(int **input_matrix, int **A_matrix, int size);
+
 int main(){
 	
 	int node_number = 4;
 	
 	//int **graph;
+	int user_input;
 	
 	cout << "Input node number" << endl;
 	//cin >> matrix_size;
 	
-	int graph[node_number][node_number];
+	int input_matrix[node_number][node_number];
+	
+	
 	
 	for(int row = 0; row < node_number; row++){
 		for(int column = 0; column < node_number; column++){
-			cout << "Enter A1 [";
+			cout << "Enter A1 [" << row << ", " << column << "]" << endl;
+			cin >> user_input;
+			input_matrix[row][column] = user_input;
 		}
+	}
+	
+	if(node_number == 2)
+	{
+		int A1_matrix[node_number][node_number];
+		
+		
+		for(int row = 0; row < node_number; row++){
+			for(int column = 0; column < node_number; column++){
+				
+			}
+		}
+	
+		int A2_matrix[node_number][node_number];
+		
+		for(int row = 0; row < node_number; row++){
+			for(int column = 0; column < node_number; column++){
+				A1_matrix[row][column] = input_matrix[row][column];
+			}
+		}
+		
+		
+	}
+	
+	if(node_number == 3)
+	{
+		int A1_matrix[node_number][node_number];
+		int A2_matrix[node_number][node_number];
+		
+		int A3_matrix[node_number][node_number];
+	}
+	
+	if(node_number == 4)
+	{
+		int A1_matrix[node_number][node_number];
+		int A2_matrix[node_number][node_number];
+	
+		int A3_matrix[node_number][node_number];
+		int A4_matrix[node_number][node_number];
+		//matrix_multiplication(input_matrix, A2_matrix, node_number);
 	}
 	
 	return EXIT_SUCCESS;
 }
 
-
+//A_matrix can be A2, A3, or A4 depending on our nodes
+void matrix_multiplication(int **input_matrix, int **A_matrix, int size)
+{
+	
+}
 
