0d8af88b4e66f6f7d147ac647bc92d247c8876b1
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2021-04-29 14:22:09 -0400

    Coding A12

--- A12_Graphs/Reachability_Matrix.cpp
+++ A12_Graphs/Reachability_Matrix.cpp
@@ -14,15 +14,19 @@
 
 using namespace std;
 
-void matrix_multiplication(int **input_matrix, int **A_matrix, int size);
+char get_command();
+void print_menu();
+void print_2x2_matrix(int matrix[2][2]);
+void print_3x3_matrix(int matrix[3][3]);
+void print_4x4_matrix(int matrix[4][4]);
 
 int main(){
 	
-	int node_number = 4;
+	char command;
+	int node_number = 2;
 	
-	//int **graph;
+	
 	int user_input;
-	
 	cout << "Input node number" << endl;
 	//cin >> matrix_size;
 	
@@ -40,32 +44,93 @@ int main(){
 	
 	if(node_number == 2)
 	{
-		int A1_matrix[node_number][node_number];
 		
+		int boundary_row = 0;
+		int boundary_column = 0;
 		
-		for(int row = 0; row < node_number; row++){
-			for(int column = 0; column < node_number; column++){
-				
-			}
-		}
-	
-		int A2_matrix[node_number][node_number];
-		
+		int A1_matrix[2][2];
+		int A2_matrix[2][2];
+		//A1_matrix will copy input_matrix
 		for(int row = 0; row < node_number; row++){
 			for(int column = 0; column < node_number; column++){
 				A1_matrix[row][column] = input_matrix[row][column];
 			}
 		}
+	
 		
+		/*We will perform matrix multiplication for A2*/
+		for(int row = 0; row < node_number; row++){
+			for(int column = 0; column < node_number; column++){
+				A2_matrix[row][column] = 0;
+				boundary_row = A1_matrix[row][column];
+				
+				/*down column will control the movement down a column. 
+				 * this implies that it is really moving down the rows one column at a time
+				 * the name should be less confusing now with this comment*/
+				for(int down_column = 0; down_column < node_number; down_column++){
+					boundary_column = A1_matrix[down_column][column];
+					A2_matrix[row][column] += boundary_row * boundary_column;
+				}
+				
+			}
+		}
 		
+		cout << "A2" << endl;
+		print_2x2_matrix(A2_matrix);
 	}
 	
 	if(node_number == 3)
 	{
-		int A1_matrix[node_number][node_number];
-		int A2_matrix[node_number][node_number];
+		int A1_matrix[3][3];
+		int A2_matrix[3][3];
+		int A3_matrix[3][3];
 		
-		int A3_matrix[node_number][node_number];
+		int boundary_row = 0;
+		int boundary_column = 0;
+		
+		
+		//A1_matrix will copy input_matrix
+		for(int row = 0; row < node_number; row++){
+			for(int column = 0; column < node_number; column++){
+				A1_matrix[row][column] = input_matrix[row][column];
+			}
+		}
+	
+		/*We will perform matrix multiplication for A2*/
+		for(int row = 0; row < node_number; row++){
+			for(int column = 0; column < node_number; column++){
+				A2_matrix[row][column] = 0;
+				boundary_row = A1_matrix[row][column];
+				
+				/*down column will control the movement down a column. 
+				 * this implies that it is really moving down the rows one column at a time
+				 * the name should be less confusing now with this comment*/
+				for(int down_column = 0; down_column < node_number; down_column++){
+					boundary_column = A1_matrix[down_column][column];
+					A2_matrix[row][column] += boundary_row * boundary_column;
+				}
+				
+			}
+		}
+		
+		/*We will perform matrix multiplication for A3*/
+		for(int row = 0; row < node_number; row++){
+			for(int column = 0; column < node_number; column++){
+				A2_matrix[row][column] = 0;
+				boundary_row = A2_matrix[row][column];
+				
+				/*down column will control the movement down a column. 
+				 * this implies that it is really moving down the rows one column at a time
+				 * the name should be less confusing now with this comment*/
+				for(int down_column = 0; down_column < node_number; down_column++){
+					boundary_column = A1_matrix[down_column][column];
+					A3_matrix[row][column] += boundary_row * boundary_column;
+				}
+				
+			}
+		}
+		
+		
 	}
 	
 	if(node_number == 4)
@@ -78,12 +143,38 @@ int main(){
 		//matrix_multiplication(input_matrix, A2_matrix, node_number);
 	}
 	
+	//printing outputs
+	
+	//print_matrix(*input_matrix[node_number], node_number);
+
+	
 	return EXIT_SUCCESS;
 }
 
-//A_matrix can be A2, A3, or A4 depending on our nodes
-void matrix_multiplication(int **input_matrix, int **A_matrix, int size)
+void print_menu()
 {
-	
+    cout << "\n-----Press a number for the following menu option-----\n" << endl;
+    cout << "0. Exit Program" << endl;
+    cout << "1. Enter graph data" << endl;
+    cout << "2. Print Outputs" << endl;
+}
+
+char get_command()
+{
+    char command;
+
+    cout << ">";
+    cin >> command;
+
+    return (toupper(command));
+}
+
+void print_2x2_matrix(int A2_matrix[2][2]){
+	for(int row = 0; row < 2; row++){
+		for(int column = 0; column < 2; column++){
+			cout << A2_matrix[row][column] << " ";
+		}
+		cout << endl;
+		}
 }
 
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ z_LB_a11 - Debug ]----------"
-	@cd "z_LB_a11" && "$(MAKE)" -f  "z_LB_a11.mk"
+	@echo "----------Building project:[ A12_Graphs - Debug ]----------"
+	@cd "A12_Graphs" && "$(MAKE)" -f  "A12_Graphs.mk"
 clean:
-	@echo "----------Cleaning project:[ z_LB_a11 - Debug ]----------"
-	@cd "z_LB_a11" && "$(MAKE)" -f  "z_LB_a11.mk" clean
+	@echo "----------Cleaning project:[ A12_Graphs - Debug ]----------"
+	@cd "A12_Graphs" && "$(MAKE)" -f  "A12_Graphs.mk" clean
